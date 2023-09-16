a16f7d811b024f6eec939fb292962fd9b85bec3a
69LordCrazyCoder69 <145223458+69LordCrazyCoder69@users.noreply.github.com>
2023-09-16 19:03:27 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+function convert() {
+  const amount = parseFloat(document.getElementById('amount').value);
+  const fromCurrency = document.getElementById('fromCurrency').value;
+  const toCurrency = document.getElementById('toCurrency').value;
+  
+  // Replace these with your own conversion rates
+  const conversionRates = {
+    Vodra: 1,
+    Para: 5,
+    // Add more conversion rates for different currencies
+  };
+
+  const convertedAmount = amount * conversionRates[toCurrency] / conversionRates[fromCurrency];
+  document.getElementById('result').innerText = `Converted Amount: ${convertedAmount.toFixed(2)} ${toCurrency}`;
+}
